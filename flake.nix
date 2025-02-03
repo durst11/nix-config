386e4056c1d2ccc37f9bc7d7dc2262b19be4a18d
Jeremy <durst.11@protonmail.com>
2025-02-03 15:23:30 -0500

    moving to branch nvf

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nvf.url = "github:notashelf/nvf";
 
     nixos-cosmic = {
       url = "github:lilyinstarlight/nixos-cosmic";
--- nvf-configuration.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, lib, ... }:
-
-{
-  vim.theme.enable = true;
-  vim.theme.name = "gruvbox";
-  vim.theme.style = "dark";
-
-  vim.languages.nix.enable = true;
-}
